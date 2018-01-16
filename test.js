8547798bc67564f8204cf85420e0ea9314e7205e
Your Name <email@example.com>
2018-01-16 14:58:03 +0800

    webpack

---
+++ test.js
@@ -0,0 +1,6 @@
+require("./word");
+require("style-loader!css-loader!./1.css");
+function hello(str){
+  alert(str)
+}
+hello("abc111");
---
+++ word.js
@@ -0,0 +1,4 @@
+function word(){
+  return {}
+}
+alert(word());
